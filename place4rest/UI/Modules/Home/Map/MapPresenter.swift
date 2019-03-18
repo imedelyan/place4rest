3dee09ca662cc3f40af0ac6595aef54807c1b442
Igor Medelian <igor.medelyan@ekreative.com>
2019-03-18 18:22:43 +0200

    Added map presenter and props.

--- place4rest/UI/Modules/Home/Map/MapPresenter.swift
+++ place4rest/UI/Modules/Home/Map/MapPresenter.swift
@@ -6,29 +6,107 @@
 //  Copyright © 2019 imedelian. All rights reserved.
 //
 
+import Mapbox
 import PromiseKit
 
-final class MapPresenter {
+final class MapPresenter: NSObject {
 
     // MARK: - Dependencies
     private let placesService: PlacesService
     weak var view: MapView!
 
+    // MARK: - Variables
+    lazy var props: MapViewController.Props = .init(mapItems: [],
+                                                    layersButtonAction: .nop,
+                                                    locateButtonAction: Command(action: showUserLocation),
+                                                    searchButtonAction: .nop,
+                                                    filterButtonAction: .nop,
+                                                    filterType: .allPlaces,
+                                                    layerType: .satellite,
+                                                    userLocation: .notSet,
+                                                    currentLocation: .notSet,
+                                                    visibleCoordinateBounds: MGLCoordinateBounds())
+
+    // MARK: - Init
     init(placesService: PlacesService) {
         self.placesService = placesService
     }
 
+    // MARK: - MapView
     func viewWasLoaded() {
-        view.setLoading(hidden: false)
+//        view.setLoading(hidden: false)
         placesService
             .getAllPlaces()
             .ensure { [weak self] in
-                self?.view.setLoading(hidden: true)
+//                self?.view.setLoading(hidden: true)
             }.done { [weak self] places in
-                self?.view.show(places: places)
+                guard let self = self else { return }
+                let mapItems = places.map {
+                    MapViewController.Props.MapItem(place: $0, select: Command(action: {}))
+                }
+                self.props = .init(mapItems: mapItems,
+                              layersButtonAction: self.props.layersButtonAction,
+                              locateButtonAction: self.props.locateButtonAction,
+                              searchButtonAction: self.props.searchButtonAction,
+                              filterButtonAction: self.props.filterButtonAction,
+                              filterType: self.props.filterType,
+                              layerType: self.props.layerType,
+                              userLocation: self.props.userLocation,
+                              currentLocation: self.props.currentLocation,
+                              visibleCoordinateBounds: self.props.visibleCoordinateBounds)
+                self.view.render(props: self.props)
             }.catch { [weak self] in
                 print($0.localizedDescription)
-                self?.view.display(errorMessage: $0.localizedDescription)
+//                self?.view.display(errorMessage: $0.localizedDescription)
         }
+    }
+
+    private func showUserLocation() {
+        props = .init(mapItems: props.mapItems,
+                      layersButtonAction: props.layersButtonAction,
+                      locateButtonAction: props.locateButtonAction,
+                      searchButtonAction: props.searchButtonAction,
+                      filterButtonAction: props.filterButtonAction,
+                      filterType: props.filterType,
+                      layerType: props.layerType,
+                      userLocation: props.userLocation,
+                      currentLocation: props.userLocation,
+                      visibleCoordinateBounds: props.visibleCoordinateBounds)
+        view?.render(props: props)
+    }
+}
+
+// MARK: - MGLMapViewDelegate
+extension MapPresenter: MGLMapViewDelegate {
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        return true
+    }
+
+    func mapView(_ mapView: MGLMapView, didSelect annotation: MGLAnnotation) {
+        let camera = MGLMapCamera(lookingAtCenter: annotation.coordinate, acrossDistance: 4500, pitch: 15, heading: 180)
+        mapView.fly(to: camera, withDuration: 4, peakAltitude: 3000, completionHandler: nil)
+    }
+
+    func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
+        let userLocation = MapViewController.Props.CameraLocation(coordinate: mapView.userLocation!.coordinate,
+                                                                  zoomLevel: 5)
+        props = .init(mapItems: props.mapItems,
+                      layersButtonAction: props.layersButtonAction,
+                      locateButtonAction: props.locateButtonAction,
+                      searchButtonAction: props.searchButtonAction,
+                      filterButtonAction: props.filterButtonAction,
+                      filterType: props.filterType,
+                      layerType: props.layerType,
+                      userLocation: userLocation,
+                      currentLocation: props.userLocation,
+                      visibleCoordinateBounds: props.visibleCoordinateBounds)
+        view?.render(props: props)
+    }
+}
+
+// MARK: - Props Factory
+extension MapPresenter {
+    private func makeProps() {
+
     }
 }
--- place4rest/UI/Modules/Home/Map/MapViewController.swift
+++ place4rest/UI/Modules/Home/Map/MapViewController.swift
@@ -9,7 +9,7 @@
 import Mapbox
 import UIKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MapView {
 
     // MARK: - IBOutlet
     @IBOutlet private weak var mapView: MGLMapView!
@@ -21,6 +21,16 @@ class MapViewController: UIViewController {
     var presenter: MapPresenter!
 
     // MARK: - Variables
+    var props: MapViewController.Props = .init(mapItems: [],
+                                               layersButtonAction: .nop,
+                                               locateButtonAction: .nop,
+                                               searchButtonAction: .nop,
+                                               filterButtonAction: .nop,
+                                               filterType: .allPlaces,
+                                               layerType: .satellite,
+                                               userLocation: .notSet,
+                                               currentLocation: .notSet,
+                                               visibleCoordinateBounds: MGLCoordinateBounds())
     private let mapStyles: [URL?] = [
         MGLStyle.streetsStyleURL,
         URL(string: "mapbox://styles/imedelian/cjs3aet6z19qe1ft85b2xva57"),
@@ -30,15 +40,26 @@ class MapViewController: UIViewController {
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView.styleURL = MGLStyle.satelliteStyleURL
         mapView.compassView.isHidden = true
         mapView.logoView.isHidden = true
         mapView.attributionButton.isHidden = true
+        mapView.delegate = presenter
 
         presenter.viewWasLoaded()
+    }
 
-        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPressOnMap))
-        mapView.addGestureRecognizer(longPressRecognizer)
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        show(places: props.mapItems)
+        if mapView.styleURL != mapStyles[props.layerType.rawValue] {
+            mapView.styleURL = mapStyles[props.layerType.rawValue]
+        }
+        mapView.setCenter(props.currentLocation.coordinate, zoomLevel: props.currentLocation.zoomLevel, animated: true)
+    }
+
+    func render(props: Props) {
+        self.props = props
+        self.view.setNeedsLayout()
     }
 
     // MARK: - IBAction
@@ -59,18 +80,24 @@ class MapViewController: UIViewController {
     }
 
     @IBAction private func locateButtonAction(_ sender: Any) {
-        mapView.setCenter((mapView.userLocation?.coordinate)!, zoomLevel: 7, animated: true)
+        props.locateButtonAction.perform()
     }
 
     @IBAction private func searchButtonAction(_ sender: Any) {
 
+        props.searchButtonAction.perform(with: "Test")
     }
 
     @IBAction private func filterButtonAction(_ sender: Any) {
 
+        props.filterButtonAction.perform(with: .allPlaces)
     }
 
     // MARK: - Anotations
+    private func show(places: [Props.MapItem]) {
+        places.forEach { addAnnotation(for: $0.place) }
+    }
+
     private func addAnnotation(for place: Place) {
         let annotation = MGLPointAnnotation()
         annotation.coordinate = CLLocationCoordinate2D(latitude: place.latitude, longitude: place.longitude)
@@ -86,49 +113,51 @@ class MapViewController: UIViewController {
 //        annotation.subtitle = "Hello!"
         mapView.addAnnotation(annotation)
     }
-
-    @objc private func longPressOnMap(_ recognizer: UILongPressGestureRecognizer) {
-        let longPressScreenCoordinates = recognizer.location(in: mapView)
-        let longPressMapCoordinates = mapView.convert(longPressScreenCoordinates, toCoordinateFrom: mapView)
-
-        addAnnotation(coordinates: longPressMapCoordinates)
-    }
-}
-
-extension MapViewController: MGLMapViewDelegate {
-    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
-        return true
-    }
-
-    func mapView(_ mapView: MGLMapView, didSelect annotation: MGLAnnotation) {
-        let camera = MGLMapCamera(lookingAtCenter: annotation.coordinate, acrossDistance: 4500, pitch: 15, heading: 180)
-        mapView.fly(to: camera, withDuration: 4, peakAltitude: 3000, completionHandler: nil)
-    }
-
-    func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
-        mapView.setCenter((mapView.userLocation?.coordinate)!, zoomLevel: 3, animated: true)
-    }
 }
 
 // MARK: - MapView
 protocol MapView: class {
-    func show(places: [Place])
-    func display(errorMessage: String?)
-    func setLoading(hidden: Bool)
+    func render(props: MapViewController.Props)
 }
 
-extension MapViewController: MapView {
-    func show(places: [Place]) {
-        places.forEach { addAnnotation(for: $0) }
-    }
+// MARK: - Props
+extension MapViewController {
+    struct Props {
+        let mapItems: [MapItem]
+        let layersButtonAction: CommandWith<LayerType>
+        let locateButtonAction: Command
+        let searchButtonAction: CommandWith<String>
+        let filterButtonAction: CommandWith<FilterType>
+        let filterType: FilterType
+        let layerType: LayerType
+        let userLocation: CameraLocation
+        let currentLocation: CameraLocation
+        let visibleCoordinateBounds: MGLCoordinateBounds
 
-    func display(errorMessage: String?) {
-//        errorLabel.text = errorMessage
-    }
+        struct MapItem {
+            let place: Place
+            let select: Command
+        }
 
-    func setLoading(hidden: Bool) {
-//        hidden
-//            ? hideSpinner()
-//            : showSpinner()
+        enum FilterType {
+            case hostel
+            case wildPlace
+            case allPlaces
+        }
+
+        enum LayerType: Int {
+            case street
+            case terrain
+            case satellite
+        }
+
+        struct CameraLocation {
+            let coordinate: CLLocationCoordinate2D
+            let zoomLevel: Double
+
+            static var notSet: CameraLocation {
+                return CameraLocation(coordinate: CLLocationCoordinate2D(), zoomLevel: 0)
+            }
+        }
     }
 }
