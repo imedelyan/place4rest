537eaab7e9e8b9ce5821678a0be788ef02ce0ff0
Igor Medelyan <igor.medelyan@ekreative.com>
2019-05-04 13:04:03 +0300

    AddPlace button action.

--- place4rest/Modules/Add Place/Add Place/AddPlaceViewController.swift
+++ place4rest/Modules/Add Place/Add Place/AddPlaceViewController.swift
@@ -13,12 +13,7 @@ class AddPlaceViewController: UIViewController {
     // MARK: - Dependencies
     var navigator: AddPlaceNavigator!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
+    // MARK: - Life cycle
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated)
@@ -29,14 +24,8 @@ class AddPlaceViewController: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - IBAction
+    @IBAction func didTapAddPlaceButton(_ sender: Any) {
+        navigator.navigate(to: .chooseCategories)
     }
-    */
-
 }
